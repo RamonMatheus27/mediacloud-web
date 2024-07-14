1db6d03a4fd53684f164a5977b6dd67b5819007b
Luisfelipe114 <cabralluis994@gmail.com>
2024-07-14 16:22:16 -0300

    feat: mostrando mensagens de erro e fazendo validacao no front

--- meu-app/src/auth/login/Login.js
+++ meu-app/src/auth/login/Login.js
@@ -1,55 +1,106 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../../App.css';
+import '../AuthForm.css';
 import authService from '../../services/AuthService';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function Login() {
   const [user, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [userError, setUserError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
   const [error, setError] = useState(null);
+  const [hasErrors, setHasErrors] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   const handleLogin = (e) => {
     e.preventDefault();
 
+    if (hasErrors || loading) {
+      return;
+    }
+
+    setLoading(true);
+
     authService.login(user, password)
       .then(response => {
         console.log('Login bem-sucedido:', response);
+        setLoading(false);
       })
       .catch(error => {
         console.error('Erro ao fazer login:', error);
-        setError('Credenciais inválidas. Por favor, tente novamente.');
+        setError(error.response.data.message || 'Credenciais inválidas. Por favor, tente novamente.');
+        setLoading(false);
       });
+  };
+
+  const validateUser = () => {
+    const hasError = user.length === 0;
+    setUserError(hasError);
+    updateFormErrorState();
+  };
+
+  const validatePassword = () => {
+    const hasError = password.length === 0;
+    setPasswordError(hasError);
+    updateFormErrorState();
+  };
+
+  const updateFormErrorState = () => {
+    setHasErrors(userError || passwordError);
   };
 
   return (
     <div className="login-container">
-      <h1>Login Page</h1>
-      {error && <p className="error-message">{error}</p>}
-      <form onSubmit={handleLogin}>
-        <div className="form-control">
-          <label>Username</label>
-          <input
-            type="text"
-            value={user}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-control">
-          <label>Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-      <p>Não tem uma conta? <Link to="/register">Registre-se aqui</Link></p>
+      <div className="form-container">
+        <h1>Entrar</h1>
+        {error && (
+          <div className="error-message">
+            {Array.isArray(error) ? error.map((err, index) => <p key={index}>{err}</p>) : <p>{error}</p>}
+          </div>
+        )}
+        <form onSubmit={handleLogin}>
+          <div className="form-control">
+            <label>Usuário</label>
+            <input
+              type="text"
+              value={user}
+              onChange={(e) => {
+                setUsername(e.target.value);
+                validateUser();
+              }}
+              onBlur={validateUser}
+              required
+            />
+            {userError && (
+              <p className="error-text">Usuário é obrigatório.</p>
+            )}
+          </div>
+          <div className="form-control">
+            <label>Senha</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+                validatePassword();
+              }}
+              onBlur={validatePassword}
+              required
+            />
+            {passwordError && (
+              <p className="error-text">Senha é obrigatória.</p>
+            )}
+          </div>
+          <button type="submit" disabled={hasErrors || loading}>
+            {loading ? <i className="fa fa-spinner fa-spin"></i> : 'Entrar'}
+          </button>
+        </form>
+        <p>Não tem uma conta? <Link to="/register">Registre-se aqui</Link></p>
+      </div>
     </div>
   );
 }
 
 export default Login;
-
--- meu-app/src/auth/register/Register.js
+++ meu-app/src/auth/register/Register.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../../App.css';
+import '../AuthForm.css';
 import authService from '../../services/AuthService';
 
 function Register() {
@@ -7,11 +9,20 @@ function Register() {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [usernameError, setUsernameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [error, setError] = useState(null);
+  const [hasErrors, setHasErrors] = useState(true);
 
   const navigate = useNavigate();
 
   const handleRegister = (e) => {
     e.preventDefault();
+
+    if (hasErrors) {
+      return;
+    }
 
     authService.register(name, username, email, password)
       .then(response => {
@@ -20,51 +31,107 @@ function Register() {
       })
       .catch(error => {
         console.error('Erro ao registrar usuário:', error);
+        const errorMessage = Array.isArray(error.response.data.message)
+          ? error.response.data.message
+          : 'Erro ao registrar usuário.';
+        setError(errorMessage);
       });
+  };
+
+  const validateUsername = () => {
+    const hasError = username.length < 4 || username.length > 20 || !/^[a-zA-Z0-9]*$/.test(username);
+    setUsernameError(hasError);
+    updateFormErrorState();
+  };
+
+  const validateEmail = () => {
+    const hasError = !/\S+@\S+\.\S+/.test(email);
+    setEmailError(hasError);
+    updateFormErrorState();
+  };
+
+  const validatePassword = () => {
+    const hasError = password.length < 6 || !/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z$*&@#]{6,}/.test(password);
+    setPasswordError(hasError);
+    updateFormErrorState();
+  };
+
+  const updateFormErrorState = () => {
+    setHasErrors(usernameError || emailError || passwordError);
   };
 
   return (
     <div className="register-container">
-      <h1>Register Page</h1>
-      <form onSubmit={handleRegister}>
-      <div className="form-control">
-          <label>Name</label>
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-control">
-          <label>Username</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-control">
-          <label>Email</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-control">
-          <label>Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <button type="submit">Register</button>
-      </form>
+      <div className="form-container">
+        <h1>Cadastro</h1>
+        {error && (
+          <div className="error-message">
+            {Array.isArray(error) ? error.map((err, index) => <p key={index}>{err}</p>) : <p>{error}</p>}
+          </div>
+        )}
+        <form onSubmit={handleRegister}>
+          <div className="form-control">
+            <label>Nome</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label>Usuário</label>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+                validateUsername();
+              }}
+              onBlur={validateUsername}
+              required
+            />
+            {usernameError && (
+              <p className="error-text">Usuário deve conter de 4 a 20 caracteres e não pode conter caracteres especiais.</p>
+            )}
+          </div>
+          <div className="form-control">
+            <label>Email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+                validateEmail();
+              }}
+              onBlur={validateEmail}
+              required
+            />
+            {emailError && (
+              <p className="error-text">Email inválido.</p>
+            )}
+          </div>
+          <div className="form-control">
+            <label>Senha</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+                validatePassword();
+              }}
+              onBlur={validatePassword}
+              required
+            />
+            {passwordError && (
+              <p className="error-text">Senha deve ter pelo menos 6 caracteres e conter um dígito, uma letra minúscula e uma letra maiúscula.</p>
+            )}
+          </div>
+          <button type="submit" disabled={hasErrors}>Cadastrar</button>
+        </form>
+      </div>
     </div>
   );
 }
